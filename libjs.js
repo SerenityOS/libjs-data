c5d9885834d216ecc143cc44dbcf1f527d16d68b
BuggieBot <buggiebot@serenityos.org>
2023-12-10 08:49:45 +0000

    Deploying to libjs-wasm from @ SerenityOS/serenity@463931384df323f51535dafc6e7c9f61b2e0da6b 🚀

--- libjs.js
+++ libjs.js
@@ -4333,8 +4333,8 @@ var dynCall_viiji = Module["dynCall_viiji"] = createExportWrapper("dynCall_viiji
 /** @type {function(...*):?} */
 var dynCall_jiji = Module["dynCall_jiji"] = createExportWrapper("dynCall_jiji");
 
-var ___start_em_js = Module['___start_em_js'] = 11356596;
-var ___stop_em_js = Module['___stop_em_js'] = 11356687;
+var ___start_em_js = Module['___start_em_js'] = 11356468;
+var ___stop_em_js = Module['___stop_em_js'] = 11356559;
 function invoke_vi(index,a1) {
   var sp = stackSave();
   try {
@@ -4949,4 +4949,4 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
     emrun_register_handlers();
   }
 }
-Module.SERENITYOS_COMMIT = "6a4e3d90023b529c70e297201156530187fda4a1";
+Module.SERENITYOS_COMMIT = "463931384df323f51535dafc6e7c9f61b2e0da6b";
