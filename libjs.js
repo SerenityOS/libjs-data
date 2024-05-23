34adeea4a0fd2f41afea0c22f13cca4a3a914395
BuggieBot <buggiebot@serenityos.org>
2024-05-23 02:21:58 +0000

    Deploying to libjs-wasm from @ SerenityOS/serenity@d2b30072485c300598f66bf00850835991d9dec2 🚀

--- libjs.js
+++ libjs.js
@@ -4952,4 +4952,4 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
     emrun_register_handlers();
   }
 }
-Module.SERENITYOS_COMMIT = "5b17091a4bc3020ddf0e0f310874b4be0c8f393c";
+Module.SERENITYOS_COMMIT = "d2b30072485c300598f66bf00850835991d9dec2";
