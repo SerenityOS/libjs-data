03f5898ce4f61b714eafd2fd66b99df051b828b7
BuggieBot <buggiebot@serenityos.org>
2026-02-02 11:34:46 +0000

    Deploying to libjs-wasm from @ SerenityOS/serenity@a3fda9ad04c4ec3dc8dba414b856b392ef9940d9 🚀

--- libjs.js
+++ libjs.js
@@ -28,7 +28,7 @@ if (Module['ENVIRONMENT']) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /home/runner/work/_temp/84bdde9f-bfe0-4bb9-aac3-ac93565e1c56/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// include: /home/runner/work/_temp/46b62d5c-2c30-4ab2-87ef-406898940944/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -49,7 +49,7 @@ if (typeof window == 'object') {
     Module['arguments'] = [];
   }
 }
-// end include: /home/runner/work/_temp/84bdde9f-bfe0-4bb9-aac3-ac93565e1c56/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// end include: /home/runner/work/_temp/46b62d5c-2c30-4ab2-87ef-406898940944/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -5036,7 +5036,7 @@ run();
 
 // end include: postamble.js
 
-// include: /home/runner/work/_temp/84bdde9f-bfe0-4bb9-aac3-ac93565e1c56/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// include: /home/runner/work/_temp/46b62d5c-2c30-4ab2-87ef-406898940944/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -5132,6 +5132,6 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
     emrun_register_handlers();
   }
 }
-// end include: /home/runner/work/_temp/84bdde9f-bfe0-4bb9-aac3-ac93565e1c56/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// end include: /home/runner/work/_temp/46b62d5c-2c30-4ab2-87ef-406898940944/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 
 Module.SERENITYOS_COMMIT = "a3fda9ad04c4ec3dc8dba414b856b392ef9940d9";
