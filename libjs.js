0e0423642c01434f331f4e6be509ea72f59bd456
BuggieBot <buggiebot@serenityos.org>
2026-02-26 17:58:52 +0000

    Deploying to libjs-wasm from @ SerenityOS/serenity@7791306bf4e8afa29c0a06b0780b9c8fe58c7d08 🚀

--- libjs.js
+++ libjs.js
@@ -28,7 +28,7 @@ if (Module['ENVIRONMENT']) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /home/runner/work/_temp/cf4431c0-beaa-4555-87cb-1d90157a28a6/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// include: /home/runner/work/_temp/6cc23652-1af0-4551-8833-fc5791d7f27d/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -49,7 +49,7 @@ if (typeof window == 'object') {
     Module['arguments'] = [];
   }
 }
-// end include: /home/runner/work/_temp/cf4431c0-beaa-4555-87cb-1d90157a28a6/emsdk-main/upstream/emscripten/src/emrun_prejs.js
+// end include: /home/runner/work/_temp/6cc23652-1af0-4551-8833-fc5791d7f27d/emsdk-main/upstream/emscripten/src/emrun_prejs.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -5036,7 +5036,7 @@ run();
 
 // end include: postamble.js
 
-// include: /home/runner/work/_temp/cf4431c0-beaa-4555-87cb-1d90157a28a6/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// include: /home/runner/work/_temp/6cc23652-1af0-4551-8833-fc5791d7f27d/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 /**
  * @license
  * Copyright 2013 The Emscripten Authors
@@ -5132,6 +5132,6 @@ if (typeof window == "object" && (typeof ENVIRONMENT_IS_PTHREAD == 'undefined' |
     emrun_register_handlers();
   }
 }
-// end include: /home/runner/work/_temp/cf4431c0-beaa-4555-87cb-1d90157a28a6/emsdk-main/upstream/emscripten/src/emrun_postjs.js
+// end include: /home/runner/work/_temp/6cc23652-1af0-4551-8833-fc5791d7f27d/emsdk-main/upstream/emscripten/src/emrun_postjs.js
 
-Module.SERENITYOS_COMMIT = "020b01a0142689d9089944076f23bbc9002b4bf4";
+Module.SERENITYOS_COMMIT = "7791306bf4e8afa29c0a06b0780b9c8fe58c7d08";
